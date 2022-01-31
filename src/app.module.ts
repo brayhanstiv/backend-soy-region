2a71e70297d02d95adfd851713b3d3b0202a6704
Brayhan Suarez <brayhan9505@gmail.com>
2022-01-31 16:39:56 -0500

    feat: new categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { NewsModule } from './news/news.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, NewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
