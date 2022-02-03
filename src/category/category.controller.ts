d319075590c9dbcc64b2a13cca194132b894e86f
jhoan sebastian <jhoansebax257@gmail.com>
2022-02-02 21:56:33 -0500

    feat: category controller-services

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,18 @@
-import { Controller, Get, Param, Delete, Post, Patch } from '@nestjs/common';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import {
+  Controller,
+  Get,
+  Param,
+  Delete,
+  Post,
+  Patch,
+  Res,
+  Body,
+  HttpStatus,
+  Query,
+  NotFoundException,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
 
 @Controller('/category')
@@ -6,27 +20,69 @@ export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
-  create() {
-    return this.categoryService.create();
+  async create(@Res() res, @Body() createdCategoryDto: CreateCategoryDto) {
+    const data = await this.categoryService.create(createdCategoryDto);
+    return res.status(HttpStatus.CREATED).json({
+      statusCode: HttpStatus.CREATED,
+      message: 'Category Created successfully',
+      payload: data,
+    });
   }
 
   @Get()
-  findAll() {
-    return this.categoryService.findAll();
+  async findAll(
+    @Res() res,
+    @Query('from') from = 0,
+    @Query('limit') limit = 10,
+  ) {
+    const data = await this.categoryService.findAll(from, limit);
+    const total = await this.categoryService.total();
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      payload: data,
+      total: total,
+    });
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoryService.findOne(+id);
+  async findOne(@Res() res, @Param('id') id: string) {
+    const data = await this.categoryService.findOne(id);
+    if (!data) {
+      throw new NotFoundException('Category Not Found');
+    }
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      payload: data,
+    });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string) {
-    return this.categoryService.update(+id);
+  async update(
+    @Res() res,
+    @Param('id') id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    const data = await this.categoryService.update(id, updateCategoryDto);
+    if (!data) {
+      throw new NotFoundException('Category Not Found');
+    }
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'Category updated successfully',
+      payload: data,
+    });
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoryService.remove(+id);
+  async remove(@Res() res, @Param('id') id: string) {
+    const data = await this.categoryService.remove(id);
+    if (!data) {
+      throw new NotFoundException('Category Not Found');
+    }
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'Category deleted successfully',
+      payload: data,
+    });
   }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,26 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
+import { Category } from './schemas/category.schema';
 @Injectable()
 export class CategoryService {
-  create(/* createCategoryDto: CreateCategoryDto */) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectModel(Category.name) private categoryModel: Model<Category>,
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    const createdCategory = new this.categoryModel(createCategoryDto);
+    return createdCategory.save();
   }
 
-  findAll() {
-    return `This action returns all category`;
+  async findAll(from: number, limit: number): Promise<Category[]> {
+    return this.categoryModel
+      .find({ status: true })
+      .skip(from)
+      .limit(limit)
+      .exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: string): Promise<Category> {
+    return this.categoryModel.findById(id);
   }
 
-  update(id: number /* updateCategoryDto: UpdateCategoryDto */) {
-    return `This action updates a #${id} category`;
+  async update(
+    id: string,
+    updateCategoryDto: UpdateCategoryDto,
+  ): Promise<Category> {
+    return this.categoryModel.findByIdAndUpdate(id, updateCategoryDto, {
+      new: true,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(id: string): Promise<Category> {
+    return this.categoryModel.findByIdAndDelete(id);
+  }
+
+  async total(): Promise<number> {
+    return this.categoryModel.countDocuments({ status: true });
   }
 }
